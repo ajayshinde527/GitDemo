35a142892dd26c834671013d3e2004387a8c82a4
ajay <ajayshinde527@gmail.com>
2025-06-09 23:27:40 +0530

    this is commit from Asia Timezone guy

--- src/test/java/testCases/TC_004_Search.java
+++ src/test/java/testCases/TC_004_Search.java
@@ -1,5 +1,15 @@
 package testCases;
 
+import org.testng.annotations.Test;
+
 public class TC_004_Search {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
 //implement later
 }
