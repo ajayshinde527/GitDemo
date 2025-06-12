0d06e5a9ffd1dd3f79d367881edd07739c014bc9
ajay <ajayshinde527@gmail.com>
2025-06-12 21:13:51 +0530

    updated

--- src/test/java/testCases/TC005_new.java
+++ src/test/java/testCases/TC005_new.java
@@ -4,36 +4,8 @@ import org.testng.annotations.Test;
 
 public class TC005_new {
 
-@Test
-public void nativeappAndroid() {
-	System.out.println("hi from much from gitx");
 	
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-}
-public void nativeappAndroid2() {
-	System.out.println("hi from much from gitx");
-	
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-}
-	//new
-public void iosapps() {
-	System.out.println("hi from much from gitx");
-	
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-	System.out.println("adding develop branch from gitdemo");
-}
+	}
 
-public void Americanman() {
-	System.out.println("hi from much from gitx");
-	
-	System.out.println("adding develop branch from gitx");
-	System.out.println("adding develop branch from gitx");
-	System.out.println("adding develop branch from gitx");
-}
 
-}
+
